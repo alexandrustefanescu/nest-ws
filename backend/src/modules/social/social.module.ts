1875746b4f7e43fd4a9771b482847679dc318808
Alexandru Stefanescu <stefanescualexandru1994@gmail.com>
2026-05-01 09:12:49 +0200

    feat: register PostBookmark entity and BookmarksController in SocialModule

---
+++ backend/src/modules/social/social.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomsModule } from '../rooms/rooms.module';
+import { BookmarksController } from './bookmarks.controller';
+import { PostBookmark } from './post-bookmark.entity';
+import { PostComment } from './post-comment.entity';
+import { PostLike } from './post-like.entity';
+import { SocialEngagementService } from './social-engagement.service';
+import { SocialPost } from './social-post.entity';
+import { RoomSocialPostsController, SocialPostsController } from './social-posts.controller';
+import { SocialPostsService } from './social-posts.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([SocialPost, PostComment, PostLike, PostBookmark]), RoomsModule],
+  controllers: [SocialPostsController, RoomSocialPostsController, BookmarksController],
+  providers: [SocialPostsService, SocialEngagementService],
+  exports: [SocialPostsService, SocialEngagementService],
+})
+export class SocialModule {}
